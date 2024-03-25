7d799d7985caea34732efaaaa0d02aa62d0df85d
Nikita Lavrov <nikita.lavrov2019@gmail.com>
2024-03-25 13:30:53 +0100

    Added Soundmaster and new villager trades

--- src/main/java/com/normalone/tutorialmod/TutorialMod.java
+++ src/main/java/com/normalone/tutorialmod/TutorialMod.java
@@ -3,7 +3,9 @@ package com.normalone.tutorialmod;
 import com.normalone.tutorialmod.block.ModBlocks;
 import com.normalone.tutorialmod.item.ModItemGroups;
 import com.normalone.tutorialmod.item.ModItems;
+import com.normalone.tutorialmod.util.ModCustomTrades;
 import com.normalone.tutorialmod.util.ModLootTableModifiers;
+import com.normalone.tutorialmod.villager.ModVillagers;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
@@ -20,6 +22,8 @@ public class TutorialMod implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerBlocks();
 		ModLootTableModifiers.modifyLootTables();
+		ModCustomTrades.registerCustomTrades();
+		ModVillagers.registerVillagers();
 		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
 }
